a82f20c7577d86fe4a2e4b5ff7245f7ac8c9bca3
Jonathas <jondejotam@gmail.com>
2024-06-16 18:33:12 -0300

    add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ export default {
       colors: {
         'white-transparent': 'rgba(255,255,255,0.50)',
         'violet-spotify': 'rgba(114,43,239,1)',
+        'teal-spotify': 'rgba(179,224,217,1)',
+        'footer': 'rgba(30,30,30,1)',
       },
     }
   }
